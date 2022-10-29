4a2a1c3b23c84edd2c64111e82ef0af651ae1aee
zwang1997 <zwang684@uottawa.ca>
2022-10-28 20:36:16 -0400

    UPDATE BaseValidator and Assessment Validator with more descriptive error message

--- src/validator/BaseValidator.php
+++ src/validator/BaseValidator.php
@@ -1,19 +1,16 @@
 <?php
-//require_once( ABSPATH . 'wp-content/plugins/assessment/src/interface/Validator.php' );
-//require_once( ABSPATH . 'wp-content/plugins/assessment/src/constant/constant.php' );
-require_once(__DIR__."/../interface/Validator.php");
-require_once(__DIR__."/../constant/constant.php");
+
+require_once( __DIR__ . "/../interface/Validator.php" );
+require_once( __DIR__ . "/../constant/constant.php" );
 
 use Validator\Validator;
 
 class BaseValidator implements Validator {
 
-	public function isRequired( $content, $message ): void {
-		if ( empty( $content ) ) {
-			throw new Exception( $message, UNPROCESSABLE_ENTITY_ERROR );
-		}
-	}
-
+	/**
+	 * content must in the format of abc@abc.com
+	 * @throws Exception
+	 */
 	public function isEmail( $content, $message ): void {
 		$this->isRequired( $content, $message );
 		if ( ! filter_var( $content, FILTER_VALIDATE_EMAIL ) ) {
@@ -21,6 +18,20 @@ class BaseValidator implements Validator {
 		}
 	}
 
+	/**
+	 * content must not be empty
+	 * @throws Exception
+	 */
+	public function isRequired( $content, $message, $code = UNPROCESSABLE_ENTITY_ERROR ): void {
+		if ( empty( $content ) ) {
+			throw new Exception( $message, $code );
+		}
+	}
+
+	/**
+	 * content must have minimum length of $length
+	 * @throws Exception
+	 */
 	public function minLength( $content, $length, $message ): void {
 		$this->isRequired( $content, $message );
 		if ( $content . $length < $length ) {
