6b3e5fa062f8606310eebce891f11a136c0a1967
zwang1997 <zwang684@uottawa.ca>
2022-11-04 12:44:22 -0400

    FIX typo

--- src/controller/AssessmentController.php
+++ src/controller/AssessmentController.php
@@ -14,7 +14,7 @@ require_once( dirname( __FILE__ ) . "/../constant/constant.php" );
 function createAssessmentQuestion(): void {
 	try {
 		if ( ! isset( $_POST ) ) {
-			throw new Exception( "Invalid Request Method", METHDO_NOT_ALLOWED );
+			throw new Exception( "Invalid Request Method", METHOD_NOT_ALLOWED );
 		}
 		$result = AssessmentService::createAssessmentQuestion( $_POST );
 		wp_send_json($result);
@@ -33,7 +33,7 @@ function createAssessmentQuestion(): void {
 function updateAssessmentQuestion(): void {
 	try {
 		if ( ! isset( $_POST ) ) {
-			throw new Exception( "Invalid Request Method", METHDO_NOT_ALLOWED );
+			throw new Exception( "Invalid Request Method", METHOD_NOT_ALLOWED );
 		}
 		$result = AssessmentService::updateAssessmentQuestion( $_POST );
 		wp_send_json($result);
@@ -52,7 +52,7 @@ function updateAssessmentQuestion(): void {
 function deleteAssessmentQuestion(): void {
 	try {
 		if ( ! isset( $_POST ) ) {
-			throw new Exception( "Invalid Request Method", METHDO_NOT_ALLOWED );
+			throw new Exception( "Invalid Request Method", METHOD_NOT_ALLOWED );
 		}
 		$result = AssessmentService::deleteAssessmentQuestion( $_POST );
 		wp_send_json($result);
@@ -71,7 +71,7 @@ function deleteAssessmentQuestion(): void {
 function findAllAssessmentQuestion() {
 	try {
 		if ( ! isset( $_GET ) ) {
-			throw new Exception( "Invalid Request Method", METHDO_NOT_ALLOWED );
+			throw new Exception( "Invalid Request Method", METHOD_NOT_ALLOWED );
 		}
 		$result = AssessmentService::findAllAssessmentQuestion();
 		wp_send_json($result);
